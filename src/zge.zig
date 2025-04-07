03f424dbd3e001cfbf2e72b83c824d579008abc1
Lumor Sunil <lumorsunil@gmail.com>
2025-04-07 20:16:07 +0200

    removed camera from zge.zig

--- src/zge.zig
+++ src/zge.zig
@@ -2,5 +2,4 @@ pub const physics = @import("physics.zig");
 pub const draw = @import("draw.zig");
 pub const screen = @import("screen.zig");
 pub const vector = @import("vector.zig");
-pub const camera = @import("camera.zig");
 pub const components = @import("components.zig");
